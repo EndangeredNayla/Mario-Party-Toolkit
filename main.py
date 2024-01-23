caa704fc3ef4dbdc74b0601b4642f12997e06463
Nayla Hanegan <naylahanegan@gmail.com>
2024-01-23 18:59:58 -0500

    fix bug with light mode

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ from credits import *
 from codes import *
 from items import *
 
-customtkinter.set_appearance_mode("System")  # Modes: "System" (standard), "Dark", "Light"
+customtkinter.set_appearance_mode("Dark")  # Modes: "System" (standard), "Dark", "Light"
 customtkinter.set_default_color_theme("blue")  # Themes: "blue" (standard), "green", "dark-blue"
 
 
