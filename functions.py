d38b28c989fd750c7066b851985fcd26bc764858
Nayla Hanegan <naylahanegan@gmail.com>
2024-02-04 06:05:38 -0500

    meh

--- functions.py
+++ functions.py
@@ -48,3 +48,32 @@ def check_for_updates():
 def run_update():
     url = "https://github.com/EndangeredNayla/Mario-Party-Toolkit/releases"
     webbrowser.open_new_tab(url)
+
+def createDialog(windowTitle, warn, info, buttonTxt=None):
+    completeWindow = ctk.CTkToplevel()
+    completeWindow.geometry("600x165")
+    completeWindow.title(windowTitle)
+
+    # Load success image and display it in the success window
+    img = ctk.CTkImage(Image.open(fetchResource("assets/" + warn + ".png")), size=(100, 100))
+    imgLabel = ctk.CTkLabel(completeWindow, image=img, text="")
+    imgLabel.grid(row=0, column=0, padx=10, pady=10)
+    imgLabel.image = img  # Keep a reference to the image
+
+    if buttonTxt == None:
+        pass
+    else:
+        try:
+            button = ctk.CTkButton(completeWindow, command=run_update, text=buttonTxt)
+            button.grid(row=1, column=0, padx=50, pady=10)
+        except:
+            pass
+
+    # Configure row and column weights
+    completeWindow.columnconfigure(0, weight=1)
+    completeWindow.rowconfigure(0, weight=1)
+
+    # Display success message in the success window
+    label = ctk.CTkLabel(completeWindow, text=info, font=ctk.CTkFont(size=18))
+    label.grid(row=0, column=1, padx=25, pady=10)
+    completeWindow.focus()
