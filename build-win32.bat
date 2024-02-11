344ee3d18f39d355c2044e9d683763524ba14470
Nayla Hanegan <naylahanegan@gmail.com>
2024-02-11 04:21:22 -0500

    start work at removing Intel Mac Support. Sorry :)

--- build-win32.bat
+++ build-win32.bat
@@ -1,1 +1,1 @@
-pyinstaller --onefile .\main.py --add-data "assets/*;assets/" --add-data "dependencies/*;dependencies/" --add-data "dependencies/bin/*;dependencies/bin/" --name="Mario Party Toolkit" --noconsole --icon="assets/diceBlock.ico"
+pyinstaller --onefile .\main.py --add-data "assets/*;assets/" --add-data "dependencies/win32/*;dependencies/" --add-data "dependencies/bin/*;dependencies/bin/" --name="Mario Party Toolkit" --noconsole --icon="assets/diceBlock.ico"
--- install_macos_deps.sh
+++ install_macos_deps.sh
@@ -1,5 +1,3 @@
 arch -arch arm64 brew install python@3.12 python-tk@3.12
-arch -arch x86_64 brew install python@3.12 python-tk@3.12
 
-arch -arch arm64 /opt/homebrew/Cellar/python@3.12/3.12.1_1/bin/pip3.12 install -r requirements.txt --break-system-packages
-arch -arch x86_64 /usr/local/Cellar/python@3.11/3.12.1_1/bin/pip3.12 install -r requirements.txt --break-system-packages
+/opt/homebrew/Cellar/python@3.12/3.12.1_1/bin/pip3.12 install -r requirements.txt --break-system-packages
