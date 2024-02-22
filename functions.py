f3b1696cc35283e7a9a74ee1a01afa8b0f79f634
Nayla Hanegan <naylahanegan@gmail.com>
2024-02-21 21:55:20 -0500

    more mac light/dark switch

--- functions.py
+++ functions.py
@@ -35,8 +35,8 @@ def create_image_icon(frame, image_path, row, column):
     image = Image.open(fetchResource(image_path))
     image = image.resize((48, 48))
     image_tk = ImageTk.PhotoImage(image)
-    label = tk.Label(frame, image=image_tk)
-    label.configure(bg='#323232') # Windows fix
+    label = ctk.CTkLabel(frame, image=image_tk)
+    label.configure(fg_color=['#fcfcfc', '#323232'], text="") # Windows fix
     label.image = image_tk  # Keep a reference to the image to prevent it from being garbage collected
     label.grid(row=row, column=column)
 
@@ -45,8 +45,8 @@ def create_banner(frame, image_path, row, column):
     image = Image.open(fetchResource(image_path))
     image = image.resize((200, 75))
     image_tk = ImageTk.PhotoImage(image)
-    label = tk.Label(frame, image=image_tk)
-    label.configure(bg='#2d2d2d') # Windows fix
+    label = ctk.CTkLabel(frame, image=image_tk)
+    label.configure(fg_color=['#dbdbdb', '#323232'], text="") # Windows fix
     label.image = image_tk  # Keep a reference to the image to prevent it from being garbage collected
     label.grid(row=row, column=column)
 
